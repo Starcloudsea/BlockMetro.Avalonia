5a259be2fd2d647bd028646ba13d4dd081ffee5a
Starcloudsea <starsea152@126.com>
2024-03-19 17:36:22 +0800

    添加项目文件。

---
+++ BlockMetro.Avalonia.Gallery/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BlockMetro.Avalonia.Gallery
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BlockMetro.Avalonia.UITest/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BlockMetro.Avalonia.UITest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
