e3bffbb39b8513d507baa7b9f3734fca6809aa9c
Starcloudsea <starsea152@126.com>
2024-03-20 22:04:18 +0800

    主题色切换完成啦......

--- BlockMetro.Avalonia.UITest/App.axaml.cs
+++ BlockMetro.Avalonia.UITest/App.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using BlockMetro.Avalonia;
+using BlockMetro.Avalonia.Styles;
 
 namespace BlockMetro.Avalonia.UITest
 {
@@ -10,7 +12,7 @@ namespace BlockMetro.Avalonia.UITest
         {
             AvaloniaXamlLoader.Load(this);
         }
-
+        
         public override void OnFrameworkInitializationCompleted()
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
---
+++ BlockMetro.Avalonia/Styles/MetroTheme.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Styling;
+using System;
+
+namespace BlockMetro.Avalonia.Styles
+{
+    public class MetroTheme : Style
+    {
+        public static readonly StyledProperty<string> ThemeProperty = AvaloniaProperty.Register<MetroTheme, string>(
+            "Theme", default(string));
+
+        public string Theme
+        {
+            get { return GetValue(ThemeProperty); }
+            set { SetValue(ThemeProperty, value); }
+        }
+
+        public MetroTheme()
+        {
+            ThemeProperty.Changed.AddClassHandler<MetroTheme>((x, e) => x.OnThemeChanged(e));
+        }
+
+        private void OnThemeChanged(AvaloniaPropertyChangedEventArgs e)
+        {
+            var theme = (string)e.NewValue;
+
+            var ThemeUri = theme switch
+            {
+                "Dark" => new Uri("avares://BlockMetro.Avalonia/Styles/Themes/Dark/Accent/AccentDefault.axaml", UriKind.Absolute),
+                "Light" => new Uri("avares://BlockMetro.Avalonia/Styles/Themes/Light/Accent/AccentDefault.axaml", UriKind.Absolute),
+                _ => new Uri("avares://BlockMetro.Avalonia/Styles/Themes/Light/Accent/AccentDefault.axaml", UriKind.Absolute),
+            };
+            var dict = new ResourceInclude(ThemeUri);
+            dict.Source = ThemeUri;
+            //throw new StackOverflowException("我快崩溃了啊啊啊啊啊");
+
+            // Remove the old dictionary
+            Application.Current.Resources.MergedDictionaries.Clear();
+
+            // Add the new dictionary
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+    }
+}
