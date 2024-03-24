34f3e371dee80d759f8a9c57b2068ec14327e606
Starcloudsea <starsea152@126.com>
2024-03-25 01:50:21 +0800

    写了一大堆QAQ

--- BlockMetro.Avalonia.UITest/MainWindow.axaml.cs
+++ BlockMetro.Avalonia.UITest/MainWindow.axaml.cs
@@ -1,12 +1,25 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using BlockMetro.Avalonia.Styles;
+using System;
+using System.Diagnostics;
 
 namespace BlockMetro.Avalonia.UITest
 {
     public partial class MainWindow : Window
     {
+        MetroTheme metroTheme = new MetroTheme();
         public MainWindow()
         {
+            metroTheme.Theme = ThemePropertyValue.Light;
+            metroTheme.Accent = AccentPropertyValue.BrightBlueAccent;
             InitializeComponent();
+            Debug.WriteLine(this.ActualThemeVariant);
+        }
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            metroTheme.Accent = AccentPropertyValue.GrassBlockGreenAccent;
         }
     }
 }
--- BlockMetro.Avalonia/Styles/MetroTheme.cs
+++ BlockMetro.Avalonia/Styles/MetroTheme.cs
@@ -1,46 +1,141 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Media;
 using Avalonia.Styling;
+using Microsoft.Win32;
 using System;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
 
 namespace BlockMetro.Avalonia.Styles
 {
+    public enum ThemePropertyValue
+    {
+        Light,
+        Dark
+        
+    }
+    public enum AccentPropertyValue
+    {
+        BrightBlueAccent,
+        BrightRedAccent,
+        GrassBlockGreenAccent,
+        CreeperGreenAccent,
+        GoldAccent,
+        GoldenAccent,
+        BrightOrangeYellowAccent,
+        DeepOrangeYellowAccent,
+        RustAccent,
+        LightRustAccent,
+        BrickRedAccent,
+        MediumRedAccent,
+        LightRedAccent,
+        RedAccent,
+        BrightMagentaAccent,
+        RoseRedAccent,
+        LightPurpleRedAccent,
+        PlumRedAccent,
+        LightOrchidPurpleAccent,
+        BlueAccent,
+        DeepBlueAccent,
+        PurpleShadowAccent,
+        DeepPurpleShadowAccent,
+        SoftRainbowAccent,
+        IrisFlowerAccent,
+        LightPurpleRAccent,
+        PurpleRedAccent,
+        BrightCoolBlueAccent,
+        CoolBlueAccent,
+        SeaFoamGreenAccent,
+        TealAccent,
+        LightMintAccent,
+        DeepMintAccent,
+        LightGreenAccent,
+        SportGreenAccent,
+        GrayAccent,
+        GrayBrownAccent,
+        SteelBlueAccent,
+        MetallicBlueAccent,
+        LightMossAccent,
+        MossAccent,
+        GrassGreenAccent,
+        GreenAccent,
+        SkyGrayAccent,
+        FogGrayAccent,
+        BlueGrayAccent,
+        DeepGrayAccent,
+        BrightGreenAccent,
+        FairyTeaGreenAccent,
+        DesertCamouflageAccent,
+        ProtectiveColorAccent
+
+    }
+
     public class MetroTheme : Style
     {
-        public static readonly StyledProperty<string> ThemeProperty = AvaloniaProperty.Register<MetroTheme, string>(
-            "Theme", default(string));
+        Uri ThemeUri { get; set; }
 
-        public string Theme
+        public static readonly StyledProperty<ThemePropertyValue?> ThemeProperty = AvaloniaProperty.Register<MetroTheme, ThemePropertyValue?>("Theme", null);
+        public static readonly StyledProperty<AccentPropertyValue?> AccentProperty = AvaloniaProperty.Register<MetroTheme, AccentPropertyValue?>("Accent", null);
+
+
+        public ThemePropertyValue? Theme
         {
             get { return GetValue(ThemeProperty); }
             set { SetValue(ThemeProperty, value); }
+        }
+        public AccentPropertyValue? Accent
+        {
+            get { return GetValue(AccentProperty); }
+            set { SetValue(AccentProperty, value); }
         }
 
         public MetroTheme()
         {
             ThemeProperty.Changed.AddClassHandler<MetroTheme>((x, e) => x.OnThemeChanged(e));
+            AccentProperty.Changed.AddClassHandler<MetroTheme>((x, e) => x.OnAccentChanged(e));
         }
 
         private void OnThemeChanged(AvaloniaPropertyChangedEventArgs e)
         {
-            var theme = (string)e.NewValue;
+            var theme = (ThemePropertyValue?)e.NewValue;
 
-            var ThemeUri = theme switch
+            ThemeUri = theme switch
             {
-                "Dark" => new Uri("avares://BlockMetro.Avalonia/Styles/Themes/Dark/Accent/AccentDefault.axaml", UriKind.Absolute),
-                "Light" => new Uri("avares://BlockMetro.Avalonia/Styles/Themes/Light/Accent/AccentDefault.axaml", UriKind.Absolute),
-                _ => new Uri("avares://BlockMetro.Avalonia/Styles/Themes/Light/Accent/AccentDefault.axaml", UriKind.Absolute),
+                ThemePropertyValue.Dark => new Uri("avares://BlockMetro.Avalonia/Styles/Themes/Dark/Dark.axaml", UriKind.Absolute),
+                ThemePropertyValue.Light => new Uri("avares://BlockMetro.Avalonia/Styles/Themes/Light/Light.axaml", UriKind.Absolute),
+                _ => throw new ArgumentException($"The value provided by the Theme property is not defined in {typeof(ThemePropertyValue).Namespace}.ThemePropertyValue, which may occur because of a problem in assigning a value to the Theme property."),
             };
-            var dict = new ResourceInclude(ThemeUri);
-            dict.Source = ThemeUri;
-            //throw new StackOverflowException("我快崩溃了啊啊啊啊啊");
+            var StyleUri = new Uri("avares://BlockMetro.Avalonia/Styles/ControlsStyle/ControlsStyle.axaml", UriKind.Absolute);
 
-            // Remove the old dictionary
+            var applyTheme = new ResourceInclude(ThemeUri);
+            applyTheme.Source = ThemeUri;
+
+            var applyStyle = new StyleInclude(StyleUri);
+            applyStyle.Source = StyleUri;
+            //throw new StackOverflowException("你 库 炸 了 OwO");
+
             Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Styles.Clear();
 
             // Add the new dictionary
-            Application.Current.Resources.MergedDictionaries.Add(dict);
+            Application.Current.Resources.MergedDictionaries.Add(applyTheme);
+            Application.Current.Styles.Add(applyStyle);
+        }
+        private void OnAccentChanged(AvaloniaPropertyChangedEventArgs e)
+        {
+            var accent = (AccentPropertyValue?)e.NewValue;
+            var GetAccent = Application.Current.FindResource(accent.ToString());
+
+            SolidColorBrush AccentBrush = new SolidColorBrush();
+            AccentBrush.Color = Color.Parse(GetAccent.ToString());
+
+            Application.Current.Resources.Remove("GlobalDefaultAccent");
+
+            Application.Current.Resources.Add("GlobalDefaultAccent", AccentBrush);
         }
     }
 }
